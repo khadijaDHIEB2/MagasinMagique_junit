79f8c750129c7965e83893eaf0c445ac03851cc1
khadijaDHIEB2 <khadija.benatitallah@le-campus-numerique.fr>
2023-03-23 16:13:16 +0100

    test

--- src/test/java/com/magasin/MagasinTest.java
+++ src/test/java/com/magasin/MagasinTest.java
@@ -1,5 +1,6 @@
 package com.magasin;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -14,4 +15,15 @@ class MagasinTest {
         assertEquals("fixme", app.items[0].name);
     }
 
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void updateQuality() {
+        Item[] items = new Item[] { new Item("Kryptonite", 0, 80) ,new Item("Pass VIP Concert", 0, 40),new Item("Comté", 10, 15),new Item("foo", 10, 20) };
+        Magasin target = new Magasin(items);
+        target.updateQuality();
+        assertEquals( new Item[] { new Item("Kryptonite", 0, 80) , new Item("Pass VIP Concert", -1, 0),new Item("Comté", 9, 16),new Item("foo", 9, 19)}, items);
+    }
 }
