511737ef6f2ee8c95035314abb63a730a27b1226
khadija_dh <khadija.benatitallah@le-campus-numerique.fr>
2023-03-24 09:38:17 +0100

    implementing tests to coverage 100%

--- src/test/java/com/magasin/MagasinTest.java
+++ src/test/java/com/magasin/MagasinTest.java
@@ -7,23 +7,69 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class MagasinTest {
 
-    @Test
-    void foo() {
-        Item[] items = new Item[] { new Item("foo", 0, 0) };
-        Magasin app = new Magasin(items);
-        app.updateQuality();
-        assertEquals("fixme", app.items[0].name);
-    }
+//    @Test
+//    void foo() {
+//        Item[] items = new Item[] { new Item("foo", 0, 0) };
+//        Magasin app = new Magasin(items);
+//        app.updateQuality();
+//        assertEquals("fixme", app.items[0].name);
+//    }
 
     @BeforeEach
     void setUp() {
     }
 
     @Test
-    void updateQuality() {
+    void updateQualityGeneral() {
         Item[] items = new Item[] { new Item("Kryptonite", 0, 80) ,new Item("Pass VIP Concert", 0, 40),new Item("Comté", 10, 15),new Item("foo", 10, 20) };
         Magasin target = new Magasin(items);
+
         target.updateQuality();
-        assertEquals( new Item[] { new Item("Kryptonite", 0, 80) , new Item("Pass VIP Concert", -1, 0),new Item("Comté", 9, 16),new Item("foo", 9, 19)}, items);
+        Item[] itemsExpected = new Item[] { new Item("Kryptonite", 0, 80) , new Item("Pass VIP Concert", -1, 0),new Item("Comté", 9, 16),new Item("foo", 9, 19)};
+        for (int i = 0; i<4; i++){
+            assertEquals( itemsExpected[i].toString(), items[i].toString());
+        }
     }
+
+    @Test
+    void updateQualityPassVIP() {
+        Item[] items = new Item[] { new Item("Pass VIP Concert", 12, 40)};
+        Magasin target = new Magasin(items);
+        for (int i = 0; i <15 ; i++) {
+            target.updateQuality();
+        }
+        assertEquals( new Item("Pass VIP Concert", -3, 0).toString(), items[0].toString());
+    }
+
+    @Test
+    void updateQualityKryptonite() {
+        Item[] items = new Item[] { new Item("Kryptonite", 0, 80)};
+        Magasin target = new Magasin(items);
+        for (int i = 0; i <7 ; i++) {
+            target.updateQuality();
+        }
+        assertEquals( new Item("Kryptonite", 0, 80).toString(), items[0].toString());
+    }
+
+    @Test
+    void updateQualityComte() {
+        Item[] items = new Item[] { new Item("Comté", 10, 15)};
+        Magasin target = new Magasin(items);
+        for (int i = 0; i <15 ; i++) {
+            target.updateQuality();
+        }
+        assertEquals( new Item("Comté", -5, 35).toString(), items[0].toString());
+    }
+
+    @Test
+    void updateQualityLamda() {
+        Item[] items = new Item[] { new Item("foo", 10, 15)};
+        Magasin target = new Magasin(items);
+        for (int i = 0; i <15 ; i++) {
+            target.updateQuality();
+        }
+        assertEquals( new Item("foo", -5, 0).toString(), items[0].toString());
+    }
+
+
 }
